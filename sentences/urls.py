642a89bc6b4941eaf78cd95a1cc7d1db9bf07aa0
sumanthratna <sumanthratna@gmail.com>
2019-05-26 22:09:12 -0400

    Set up basic API

---
+++ sentences/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from sentences import views
+
+urlpatterns = [
+    path('', views.endpoints),
+    path('english/<word>/', views.english, name='sentences'),
+    path('espanol/<palabra>/', views.espanol, name='sentences'),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
---
+++ sentences/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from rest_framework import generics
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+import requests
+from bs4 import BeautifulSoup
+
+from collections import OrderedDict
+
+url = "https://www.123teachme.com/translated_sentences/prep"
+
+
+@api_view(['GET'])
+def endpoints(request):
+    return Response(OrderedDict((
+        ("info", "All requests must be GET requests."),
+        ("/english/<word>/", "Fetch English sentences containing a given phrase."),
+        ("/espanol/<palabra>/", "Busca oraciónes españoles que contiene un frase dada."),
+    )))
+
+
+def get_pairs(params):
+    r = requests.get(url=url, params=params)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    pairs = []
+    for tmp in soup.find_all('div', class_='translated-sentence-pair'):
+        pair = {
+            'español': tmp.find('div', class_='spanish-sentence').contents[0].strip(),
+            'english': tmp.find('div', class_='english-sentence').contents[0].strip()
+        }
+        pairs.append(pair)
+    return pairs
+
+
+@api_view(['GET'])
+def english(request, word):
+    params = {"phrase": word, "trans_type": "e2s", "commit": "Submit"}
+    return Response(get_pairs(params))
+
+
+@api_view(['GET'])
+def espanol(request, palabra):
+    params = {"phrase": palabra, "trans_type": "s2e", "commit": "Submit"}
+    return Response(get_pairs(params))
