1abe7b1a83a4cdef4ed39fb24f3e76ef649d7fa5
Sumanth Ratna <sumanthratna@gmail.com>
2019-06-01 21:39:25 -0400

    Add multiple output formats

--- practica-api/practica_api/settings.py
+++ practica-api/practica_api/settings.py
@@ -119,3 +119,15 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework_xml.renderers.XMLRenderer',
+        'rest_framework_yaml.renderers.YAMLRenderer',
+        'rest_framework_jsonp.renderers.JSONPRenderer',
+        'rest_framework_msgpack.renderers.MessagePackRenderer',
+        'rest_framework_csv.renderers.CSVRenderer'
+    ),
+}
