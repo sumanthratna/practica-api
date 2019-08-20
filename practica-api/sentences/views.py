af1cd55756cb75559019f6af5c4c6f44185393bb
Sumanth Ratna <sumanthratna@gmail.com>
2019-08-20 14:33:24 -0400

    Remove unused imports

--- practica-api/sentences/views.py
+++ practica-api/sentences/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-from rest_framework import generics
+# from rest_framework import generics
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
